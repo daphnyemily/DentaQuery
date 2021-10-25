4800618d79c13500554b3a7c50ef124e7a6bedb1
Daphny Barbosa <daphnyemily@gmail.com>
2021-10-25 00:01:43 -0400

    basic HTML, need to update further

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/DentaQuery?retryWrites=true&w=majority', 
+    'dbName': 'DentaQuery'
+};
